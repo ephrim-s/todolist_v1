aef71b190ad86243db217a76898c51728343aa51
ephrim-s <ephrims5@gmail.com>
2024-12-29 07:43:25 +0000

    updated

--- app.js
+++ app.js
@@ -4,48 +4,86 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const app = express();
-
-const date = require(__dirname + "/date.js");
-
-const items = ["Buy Food", "Cook food","Eat Food"];
-const workItems = [];
+const mongoose = require("mongoose");
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/todolistDB");
 
-app.get("/", function(req, res){
-   
-    const day = date.getDay();
+const itemsSchema = {name: String};
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+    name: "Welcome to your todolist!"
+});
+const item2 = new Item({
+    name: "Hit the + button to add a new item."
+});
+const item3 = new Item({
+    name: "<-- Hit this to delete an item"
+});
+
+const defaultItems = [item1, item2, item3];
+
+const listSchema = {
+    name: String,
+    items: [itemsSchema]
+};
+
+const List = mongoose.model("List", listSchema);
+
+app.get("/", async (req, res) => {
     
-res.render("list", {listTitle: day, newListItems: items}); 
+    const foundItems = await Item.find({});
+
+    if (foundItems.length === 0) {
+        Item.insertMany(defaultItems);       
+    } else {
+        res.render("list", {listTitle: "Today", newListItems: foundItems});
+    }
     
+});
+
+app.get("/:customListName", async (req, res) => {
+    const customListName = req.params.customListName;
+
+    const foundList = await List.findById({customListName});
+    console.log(foundList);
+          
+        
+    
+
+    // const list = new List({
+    //     name: customListName,
+    //     items: defaultItems
+    // });
+    // list.save();
+
 });
 
 
 app.post("/", function(req, res){
         
-    const item = req.body.newItem;
+    const itemName = req.body.newItem;
     
-    if(req.body.list === "Work"){
-        workItems.push(item);
-        res.redirect("/work");
-
-    } else {
-        items.push(item);
-        res.redirect("/");
-    }
-})
-
-app.get("/work", function(req, res){
-    
-    res.render("list", {listTitle: "Work List", newListItems: workItems});
+    const item = new Item({
+        name: itemName
+    });
+    item.save();
+    res.redirect("/");
 });
 
-app.get("/about", function(req, res){
-    res.render("about");
+app.post("/delete",  async (req, res) => {
+    const checkedItemId = req.body.checkbox;
+    await Item.findByIdAndDelete(checkedItemId );
+    res.redirect("/");
 });
+
+
+
 
 app.listen(3000, function(){
     console.log("Server started on port 3000");
